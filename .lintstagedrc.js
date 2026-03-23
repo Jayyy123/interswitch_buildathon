42fa5664af1e0ccb6fca0a421f519c561578d48b
webdott <nwaforuchechukwu2007@gmail.com>
2026-03-23 16:36:45 +0100

    chore: repo init

---
+++ .lintstagedrc.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+/** @type {import('lint-staged').Config} */
+const config = {
+  'apps/ui/**/*.{ts,tsx}': [
+    (files) => {
+      const cwd = path.join(__dirname, 'apps/ui');
+      const relative = files.map((f) => path.relative(cwd, f)).join(' ');
+      return `pnpm --filter ui lint -- --fix ${relative}`;
+    },
+  ],
+  'apps/ui/**/*.{ts,tsx,json,css,md}': ['prettier --write'],
+  'apps/backend/**/*.ts': ['prettier --write'],
+};
+
+module.exports = config;
